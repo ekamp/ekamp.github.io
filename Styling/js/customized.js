716ff9be96b7289cbbab912c3c8ecd4537f704e1
ekamp <ekamp@eden.rutgers.edu>
2014-12-13 18:45:59 -0500

    Included javascript for contact information

---
+++ Styling/js/customized.js
@@ -0,0 +1,11 @@
+function onGitHubClick(){
+	window.location.replace("https://github.com/ekamp");
+}
+
+function onEmailClick(){
+	window.location.href = "mailto:user@ekamp14@gmail.com?subject=ContactFromWebSite";
+}
+
+function onLinkedInClick(){
+	window.location.replace("https://www.linkedin.com/profile/view?id=219267740&trk=nav_responsive_tab_profile");
+}
