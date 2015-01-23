ef938cbb87223750a250c3c5d602ccee92aa62c2
ekamp <ekamp@eden.rutgers.edu>
2015-01-23 16:59:14 -0500

    Adding onHover effect

--- Styling/js/customized.js
+++ Styling/js/customized.js
@@ -7,8 +7,20 @@ function onEmailClick(){
 	window.location.href = "mailto:user@ekamp14@gmail.com?subject=ContactFromWebSite";
 }
 
+/** Email Functions **/
+
 function onLinkedInClick(){
 	window.location.replace("https://www.linkedin.com/profile/view?id=219267740&trk=nav_responsive_tab_profile");
+}
+
+function onHoverEmail()
+{
+    $("#email").attr('src', 'images/aboutR.png');
+}
+
+function offHoverEmail()
+{
+    $("#menuImg").attr('src', 'images/about.png');
 }
 
 /** OnClick Functions to be used for contact information END **/
