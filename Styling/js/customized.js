d76867331525ec52fab79f9b66010a07a8f375a8
ekamp <ekamp@eden.rutgers.edu>
2015-01-23 18:24:35 -0500

    JS hover effects

--- Styling/js/customized.js
+++ Styling/js/customized.js
@@ -1,45 +1,59 @@
-/** OnClick Functions to be used for contact information START **/
+/** Github functions **/
+
 function onGitHubClick(){
 	window.location.replace("https://github.com/ekamp");
 }
+
+function fadeInBrightGitHub(){
+  $("#github").fadeOut(200, function(){
+  	$(this).load(function() { $(this).fadeIn(200); }); 
+  	$(this).attr("src", "/images/github_selected.png");
+  });
+}
+
+function fadeInOriginalGitHub(){
+	$("#github").fadeOut(200, function(){
+  	$(this).load(function() { $(this).fadeIn(200); }); 
+  	$(this).attr("src", "/images/github.png");
+  });
+}
+
+/** Email Functions **/
 
 function onEmailClick(){
 	window.location.href = "mailto:user@ekamp14@gmail.com?subject=ContactFromWebSite";
 }
 
-/** Email Functions **/
-
-function onLinkedInClick(){
-	window.location.replace("https://www.linkedin.com/profile/view?id=219267740&trk=nav_responsive_tab_profile");
-}
-
-/** OnClick Functions to be used for contact information END **/
-
-/** JQuery onHover event listener**/
-
 function fadeInBrightEmail(){
-  $("#email").fadeOut(300, function(){
-  	$(this).load(function() { $(this).fadeIn(300); }); 
+  $("#email").fadeOut(200, function(){
+  	$(this).load(function() { $(this).fadeIn(200); }); 
   	$(this).attr("src", "/images/email_selected.png");
   });
 }
 
 function fadeInOriginalEmail(){
-	$("#email").fadeOut(300, function(){
-  	$(this).load(function() { $(this).fadeIn(300); }); 
+	$("#email").fadeOut(200, function(){
+  	$(this).load(function() { $(this).fadeIn(200); }); 
   	$(this).attr("src", "/images/email.png");
   });
 }
 
-$(document).ready(function() {  
-	$("#linkedin").hover(
-		function(){
-			$( this ).fadeOut( 500 );
-  			$( this ).fadeIn( 500 );
-		});
-	// $("#emailImages").hover(
-	// 	function(){
-	// 		fadeInBrightEmail();
-	// 		fadeInOriginalEmail();
-	// 	});
-	});
+/** LinkedIn Functions **/
+
+function onLinkedInClick(){
+	window.location.replace("https://www.linkedin.com/profile/view?id=219267740&trk=nav_responsive_tab_profile");
+}
+
+function fadeInBrightLinkedIn(){
+  $("#linkedin").fadeOut(200, function(){
+  	$(this).load(function() { $(this).fadeIn(200); }); 
+  	$(this).attr("src", "/images/linkedin_selected.png");
+  });
+}
+
+function fadeInOriginalLinkedIn(){
+	$("#linkedin").fadeOut(200, function(){
+  	$(this).load(function() { $(this).fadeIn(200); }); 
+  	$(this).attr("src", "/images/linkedin.png");
+  });
+}
