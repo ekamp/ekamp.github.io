b98ea27038f1f2e78014e3e5175ee839b1c30420
ekamp <ekamp@eden.rutgers.edu>
2015-07-31 21:05:06 -0400

    Fixed hover events to replace icon with active image without fade in and out flashing

--- Styling/js/customized.js
+++ Styling/js/customized.js
@@ -1,15 +1,16 @@
-/** Github functions **/
+/** Document ready function for creation of js effects on page **/
 
-function onGitHubClick(){
-	window.location.replace("https://github.com/ekamp");
-}
+$(function(){
+  initGithubHoverEffect();
+  initEmailHoverEffect();
+  initLinkedInHoverEffect();
 
-function fadeInBrightGitHub(){
-  // $("#github").fadeOut(0, function(){
-  // 	$(this).load(function() { $(this).fadeIn(); }); 
-  // 	$(this).attr("src", "/images/github_selected.png");
-  // });
-}
+  bindGithubOnClick();
+  bindEmailOnClick();
+  bindLinkedInOnClick();
+});
+
+/** Hover effect creation functions **/
 
 function initGithubHoverEffect(){
 	$("#github").hover(function(){
@@ -19,42 +20,46 @@ function initGithubHoverEffect(){
   });
 }
 
-/** Email Functions **/
+function initEmailHoverEffect(){
+  $("#email").hover(function(){
+    $(this).attr("src", "/images/email_selected.png");
+  }, function(){
+    $(this).attr("src", "/images/email.png");
+  });
+}
+
+function initLinkedInHoverEffect(){
+  $("#linkedin").hover(function(){
+    $(this).attr("src", "/images/linkedin_selected.png");
+  }, function(){
+    $(this).attr("src", "/images/linkedin.png");
+  });
+}
+
+/** onClick creation functions **/
+
+function bindGithubOnClick(){
+  $("#github").click(onGitHubClick);
+}
+
+function bindEmailOnClick(){
+  $("#email").click(onEmailClick);
+}
+
+function bindLinkedInOnClick(){
+  $("#linkedin").click(onLinkedInClick);
+}
+
+/** onClick functions **/
+
+function onGitHubClick(){
+  window.location.replace("https://github.com/ekamp");
+}
 
 function onEmailClick(){
 	window.location.href = "mailto:user@ekamp14@gmail.com?subject=ContactFromWebSite";
 }
 
-function fadeInBrightEmail(){
-  // $("#email").fadeOut(0, function(){
-  // 	$(this).load(function() { $(this).fadeIn(); }); 
-  // 	$(this).attr("src", "/images/email_selected.png");
-  // });
-}
-
-function fadeInOriginalEmail(){
-	// $("#email").fadeOut(0, function(){
- //  	$(this).load(function() { $(this).fadeIn(); }); 
- //  	$(this).attr("src", "/images/email.png");
- //  });
-}
-
-/** LinkedIn Functions **/
-
 function onLinkedInClick(){
 	window.location.replace("https://www.linkedin.com/profile/view?id=219267740&trk=nav_responsive_tab_profile");
-}
-
-function fadeInBrightLinkedIn(){
-  // $("#linkedin").fadeOut(0, function(){
-  // 	$(this).load(function() { $(this).fadeIn(); }); 
-  // 	$(this).attr("src", "/images/linkedin_selected.png");
-  // });
-}
-
-function fadeInOriginalLinkedIn(){
-	// $("#linkedin").fadeOut(200, function(){
- //  	$(this).load(function() { $(this).fadeIn(200); }); 
- //  	$(this).attr("src", "/images/linkedin.png");
- //  });
 }
